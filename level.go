7c7840baf72440ba14aa03297877eaeafebc923c
Paul Mundt <paul.mundt@adaptant.io>
2019-12-06 17:21:27 +0100

    Add support for custom json marshal/unmarshal and stringer interfaces

--- level.go
+++ level.go
@@ -1,5 +1,11 @@
 package minimizers
 
+import (
+	"bytes"
+	"encoding/json"
+	"log"
+)
+
 type MinimizationLevel int
 
 const (
@@ -15,3 +21,53 @@ const (
 	// Data anonymization
 	MinimizationAnonymize
 )
+
+var levelStringMap = map[MinimizationLevel]string {
+	MinimizationNone:	"none",
+	MinimizationCoarse:	"coarse",
+	MinimizationFine:	"fine",
+	MinimizationAnonymize:	"anonymize",
+}
+
+var stringLevelMap = map[string]MinimizationLevel {
+	"none":		MinimizationNone,
+	"coarse":	MinimizationCoarse,
+	"fine":		MinimizationFine,
+	"anonymize":	MinimizationAnonymize,
+}
+
+func levelFromString(levelString string) MinimizationLevel {
+	if level, ok := stringLevelMap[levelString]; ok {
+		return level
+	}
+
+	return MinimizationNone
+}
+
+func (l MinimizationLevel) MarshalJSON() ([]byte, error) {
+	buf := bytes.NewBufferString(`"`)
+	buf.WriteString(l.String())
+	buf.WriteString(`"`)
+	return buf.Bytes(), nil
+}
+
+func (l *MinimizationLevel) UnmarshalJSON(b []byte) error {
+	var levelString string
+
+	err := json.Unmarshal(b, &levelString)
+	if err != nil {
+		return err
+	}
+
+	*l = levelFromString(levelString)
+	return nil
+}
+
+func (l MinimizationLevel) String() string {
+	if name, ok := levelStringMap[l]; ok {
+		return name
+	}
+
+	log.Printf("Unknown minimization level specified (%d)\n", l)
+	return ""
+}
